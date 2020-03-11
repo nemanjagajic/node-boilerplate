ac30aecd3e0c211592d61b61618d7f1a9a3d6b5f
rtojagic <rastkotojagic1996@gmail.com>
2020-03-11 13:20:04 +0100

    This fixes mongodb deprecation warnings.

--- startup/db.js
+++ startup/db.js
@@ -3,6 +3,13 @@ const config = require('config')
 
 module.exports = function() {
   const db = config.get('db')
+
+  // Options to remove deprecation warning
+  mongoose.set('useNewUrlParser', true)
+  mongoose.set('useFindAndModify', false)
+  mongoose.set('useCreateIndex', true)
+  mongoose.set('useUnifiedTopology', true)
+
   mongoose.connect(db)
     .then(() => console.log(`Connected to ${db}...`))
 }
