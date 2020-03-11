36abc5996af4e106e8f3d565dee52aa1aa8a3a8f
nemanjagajic <nemanja.gajicru96@gmail.com>
2020-03-11 17:50:38 +0100

    minor response fixes

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,12 +3,12 @@ const config = require('config')
 
 module.exports = function(req, res, next) {
   const token = req.headers.authorization
-  if (!token) return res.status(401).send('Access denied. No token provided.')
+  if (!token) return res.status(401).send({ message: 'Access denied. No token provided.' })
 
   try {
     req.user = jwt.verify(req.headers.authorization, config.get('jwtPrivateKey'))
     next()
   } catch (ex) {
-    res.status(400).send('Invalid token.')
+    res.status(400).send({ message: 'Invalid token.' })
   }
 }
